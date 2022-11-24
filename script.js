cd5b0b85406ec38f59e76914c3ab0f7a9db7d906
Q Liesch <qliesch@desire2learn.com>
2022-11-24 13:10:26 -0700

    lesson 2.7

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // Code goes here
 
-var work = function() {
-	console.log("working hard!");
-};
 
-var doWork = function(f) {
-	console.log("starting");
-	try {
-	f();
-	} catch(ex) {
-		console.log('failed');
+// "Revealing module pattern"
+var createWorker = function() {
+
+	var workCount = 0;
+
+	var task1 = function() {
+		workCount += 1;
+		console.log("task1 " + workCount);
+	};
+
+	var task2 = function() {
+		workCount += 2;
+		console.log("task2 " + workCount);
 	}
-	console.log("complete");
+
+	// defines the public API
+	return {
+		job1: task1,
+		job2: task2
+
+	}
 };
 
-doWork(work);
+
+var worker = createWorker();
+worker.job1();
+worker.job2();
+
+worker.job2();
+worker.job2();
+worker.job1();
+
+
